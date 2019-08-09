176c7b5f0dce06a2c2cb4ef9b7d7625de25bb08e
小野勇輔 <onoysamutasuku@JustinBtaskMacBook.local>
2019-08-09 11:57:11 +0900

    firstcommit

---
+++ UserListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserListApp
+//
+//  Created by 小野勇輔 on 2019/08/09.
+//  Copyright © 2019 yo-project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
