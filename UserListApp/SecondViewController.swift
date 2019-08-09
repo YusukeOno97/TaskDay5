bcc952729b1652e90da85ae73198cfefcbdeb94c
小野勇輔 <onoysamutasuku@JustinBtaskMacBook.local>
2019-08-09 13:45:08 +0900

    キャンプ生リストと情報画面のデザイン作成

---
+++ UserListApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  UserListApp
+//
+//  Created by 小野勇輔 on 2019/08/09.
+//  Copyright © 2019 yo-project. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
